451dbe2de871ea2c94a05cdfe8ebdf9a7a32e4c6
Andrzej Słomka <andrzej.slomka@newag.pl>
2025-09-19 17:12:06 +0200

    Added fs handle

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -5,3 +5,7 @@ console.log(hello);
 
 const textIn = fs.readFileSync('./txt/input.txt', 'utf8');
 console.log(textIn);
+
+const textOut = `This is what we know about avocado:\n${textIn}\nCreated on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt', textOut);
+console.log("Text written!!!");
