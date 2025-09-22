18af9aaf8886af8e007f43898c0ce4c1677865c6
Andrzej Słomka <andrzej.slomka@newag.pl>
2025-09-22 12:53:48 +0200

    Added base routeing

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -24,8 +24,19 @@ const http = require("http");
 //SERVER
 
 const server = http.createServer((req, res) => {
-    console.log(req);
-    res.end("Hello from server");
+    const pathName = req.url;
+
+    if(pathName === "/" || pathName === "/overview"){
+        res.end("This is OVERVIEW");
+    }else if(pathName === "/product"){
+        res.end("This is PRODUCT");
+    }else{
+        res.writeHead(404, {
+            'content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        })
+        res.end("<h1>Page not found!!!</h1>");
+    }
 })
 
 server.listen(8000, '127.0.0.1', () => {
