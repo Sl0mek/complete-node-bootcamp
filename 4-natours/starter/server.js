b2443529e145b827f1cddbe09920a182defe58b0
Andrzej Słomka <andrzej.slomka@newag.pl>
2025-10-14 13:54:51 +0200

    Added tour schema

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -21,6 +21,39 @@ mongoose
     console.log('DB connection successful!');
   });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Forest Hiker',
+  rating: 4.7,
+  price: 497,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(`Saved: ${doc}`);
+  })
+  .catch((err) => {
+    console.log(`Err: ${err}`);
+  });
+
 // mongoose
 //   .connect(process.env.DATABASE_LOCAL, {
 //     useNewUrlParser: true,
