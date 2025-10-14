b51274b2b55945d5755f880109bbcf69fddb8f87
Andrzej Słomka <andrzej.slomka@newag.pl>
2025-10-14 12:53:16 +0200

    Added mongoose

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,8 +1,36 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 console.log(process.env);
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connection successful!');
+  });
+
+// mongoose
+//   .connect(process.env.DATABASE_LOCAL, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//   })
+//   .then((con) => {
+//     console.log(con.connections);
+//     console.log('DB connection successful!');
+//   });
 
 const port = process.env.PORT || 3500;
 app.listen(port, () => {
