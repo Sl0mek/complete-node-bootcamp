82ac4a4ff761ca920284efd61386629cfbd00a3b
Andrzej Słomka <andrzej.slomka@newag.pl>
2026-01-03 00:26:37 +0100

    Added jwt token in controllers/authController.js

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -1,11 +1,17 @@
 const User = require('./../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const jwt = require('jsonwebtoken');
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUsesr = await User.create(req.body);
 
+  const token = jwt.sign({ id: newUsesr._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUsesr,
     },
