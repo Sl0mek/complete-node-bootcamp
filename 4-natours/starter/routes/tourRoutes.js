1302020e7c941ca22f5bd5f01421063c874659b1
Andrzej Słomka <andrzej.slomka@newag.pl>
2026-01-03 21:02:44 +0100

    Added protected route - getAllTours

--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -12,6 +12,8 @@ const {
   getMonthlyPlan,
 } = require('./../controllers/tourController');
 
+const authController = require('./../controllers/authController');
+
 const router = express.Router();
 
 // router.param('id', checkID);
@@ -19,7 +21,10 @@ const router = express.Router();
 router.route('/top-5-cheap').get(aliasToptours, getAllTours);
 router.route('/tour-stats').get(getTourStats);
 router.route('/monthly-plan/:year').get(getMonthlyPlan);
-router.route('/').get(getAllTours).post(checkBody, createTour);
+router
+  .route('/')
+  .get(authController.protect, getAllTours)
+  .post(checkBody, createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
