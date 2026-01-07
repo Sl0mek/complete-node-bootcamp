a57bec3bf8eb0575fbc1e4dfb230c538928c98c8
Andrzej Słomka <andrzej.slomka@newag.pl>
2026-01-07 14:49:51 +0100

    Added security middleware

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -5,10 +5,16 @@ const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
 const globallErrorHandler = require('./controllers/errorController');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const app = express();
 
 // MIDDLEWARE
+
+app.use(helmet());
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -25,6 +31,29 @@ app.use('/api', limiter);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
 
 // app.use((req, res, next) => {
 //   console.log('Hello form the middleware!');
