43431b46b9468563a6af99f68e31f00343d723d3
Andrzej Słomka <andrzej.slomka@newag.pl>
2026-01-07 14:19:53 +0100

    Added request limiter

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -4,6 +4,7 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
 const globallErrorHandler = require('./controllers/errorController');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 
@@ -12,6 +13,16 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+
+app.use('/api', limiter);
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
